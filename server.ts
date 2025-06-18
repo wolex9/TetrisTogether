3110d22d9b9c691f17cc9ef2d9a75ccb67bf990f
wolex9 <76708852+wolex9@users.noreply.github.com>
2025-06-18 04:44:57 +0300

    install socket.io

--- server.ts
+++ server.ts
@@ -1,13 +1,20 @@
 import { createServer } from "http";
 import next from "next";
+import { Server } from "socket.io";
 
 const port = parseInt(process.env.PORT || "3000");
 const dev = process.env.NODE_ENV !== "production";
 const app = next({ dev, turbopack: true });
-const handle = app.getRequestHandler();
+const handler = app.getRequestHandler();
 
 app.prepare().then(() => {
-  const httpServer = createServer(handle);
+  const httpServer = createServer(handler);
+
+  const io = new Server(httpServer);
+
+  io.on("connection", (socket) => {
+    console.log(socket);
+  });
 
   httpServer.listen(port, () => {
     console.log(`Starting...\nhttp://localhost:${port}\n`);
